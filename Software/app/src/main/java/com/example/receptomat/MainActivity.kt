d4278c467aff521052a52568def76d55fefc8002
Franjo Čotić <fcotic22@student.foi.hr>
2024-11-09 14:01:07 +0100

    Dodana početna stranica aplikacije

--- Software/app/src/main/java/com/example/receptomat/MainActivity.kt
+++ Software/app/src/main/java/com/example/receptomat/MainActivity.kt
@@ -5,23 +5,21 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.constraintlayout.widget.ConstraintLayout
 
 class MainActivity : AppCompatActivity() {
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
+        val mainLayout = findViewById<ConstraintLayout>(R.id.main)
 
-
-
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(mainLayout) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
     }
-
 }
