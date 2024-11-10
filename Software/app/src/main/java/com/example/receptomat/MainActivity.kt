878df4f11a518a8f1c697ae38ba8b78dc5c724e6
Franjo Čotić <fcotic22@student.foi.hr>
2024-11-10 10:14:14 +0100

    Dodani activity za login i register

--- Software/app/src/main/java/com/example/receptomat/MainActivity.kt
+++ Software/app/src/main/java/com/example/receptomat/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.receptomat
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +17,18 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val mainLayout = findViewById<ConstraintLayout>(R.id.main)
+        val registerButton = findViewById<Button>(R.id.registerButton)
+        val loginButton = findViewById<Button>(R.id.loginButton)
+
+        registerButton.setOnClickListener {
+            val intent = Intent(baseContext, RegisterActivity::class.java)
+            startActivity(intent)
+        }
+
+        loginButton.setOnClickListener {
+            val intent = Intent(baseContext, LoginActivity::class.java)
+            startActivity(intent)
+        }
 
         ViewCompat.setOnApplyWindowInsetsListener(mainLayout) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
