07c124b2563582da9eb19176479d00e09b68a89f
dbrckan22 <dbrckan22@foi.hr>
2024-11-11 14:27:49 +0100

    Dizajniranje zaslona za prijavu, kreiranje novog zaslona i povezivanje zaslona.

--- Software/app/src/main/java/com/example/receptomat/LoginActivity.kt
+++ Software/app/src/main/java/com/example/receptomat/LoginActivity.kt
@@ -1,8 +1,11 @@
 package com.example.receptomat
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
@@ -11,7 +14,22 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_login)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+
+        val loginActivity = findViewById<ConstraintLayout>(R.id.loginActivity)
+        val prijavaButton = findViewById<Button>(R.id.login_Button)
+        val odustaniButton = findViewById<Button>(R.id.cancel_Button)
+
+        prijavaButton.setOnClickListener {
+            val intent = Intent(baseContext, ReceptiActivity::class.java)
+            startActivity(intent)
+        }
+
+        odustaniButton.setOnClickListener {
+            val intent = Intent(baseContext, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        ViewCompat.setOnApplyWindowInsetsListener(loginActivity) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
