f2694ca50d372eebb7ee5670904c3f3a5ab6fa37
Franjo Čotić <fcotic22@student.foi.hr>
2024-11-15 19:17:56 +0100

    Dobavljaju se unosi korisnika

--- Software/app/src/main/java/com/example/receptomat/RegisterActivity.kt
+++ Software/app/src/main/java/com/example/receptomat/RegisterActivity.kt
@@ -1,6 +1,10 @@
 package com.example.receptomat
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -9,6 +13,32 @@ import androidx.core.view.WindowInsetsCompat
 class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val btnRegister = findViewById<Button>(R.id.btnRegister)
+        val btnCancel = findViewById<Button>(R.id.btnCancel)
+        val txtHasAccount = findViewById<TextView>(R.id.txtHasAccount)
+
+        btnRegister.setOnClickListener {
+            val intent = Intent(baseContext, LoginActivity::class.java)
+            startActivity(intent)
+        }
+
+        btnCancel.setOnClickListener {
+            val intent = Intent(baseContext, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        txtHasAccount.setOnClickListener {
+            val intent = Intent(baseContext, LoginActivity::class.java)
+            startActivity(intent)
+        }
+
+        var txtName = findViewById<EditText>(R.id.txtName)
+        var txtUsername = findViewById<EditText>(R.id.txtUsername)
+        var txtEmail = findViewById<EditText>(R.id.txtEmail)
+        var txtPassword = findViewById<EditText>(R.id.txtPassword)
+        var txtConfirmPassword = findViewById<EditText>(R.id.txtConfirmPassword)
+
         enableEdgeToEdge()
         setContentView(R.layout.activity_register)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
