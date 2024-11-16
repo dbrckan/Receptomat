2a89e2808e8838ff222bda4c1aa77b939473c8ed
nantolic22 <nantolic22@foi.student.hr>
2024-11-16 13:59:05 +0100

    Kreiranje prikaza recepta - kreirani entitei: Meal, Recipe i RecipeAdapter za bolju organizaciju podataka - uređen HomeFragment za prikaz recepata - dodan MockDataLoader za simulaciju i prikaz podataka u aplikaciji
    Promjene omogućuju jednostavno učitavanje i prikaz recepata u aplikaciji.

---
+++ Software/app/src/main/java/com/example/receptomat/entities/Meal.kt
@@ -0,0 +1,8 @@
+package com.example.receptomat.entities
+
+enum class Meal (val displayName: String) {
+    BREAKFAST("Doručak"),
+    LUNCH("Ručak"),
+    DINNER("Večera"),
+    DESSERT("Desert");
+}
