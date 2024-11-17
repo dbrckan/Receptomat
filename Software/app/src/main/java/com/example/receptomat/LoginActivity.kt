e72efaead301e35c976f274a5a47ef85521bc3f6
Franjo Čotić <fcotic22@student.foi.hr>
2024-11-17 09:10:21 +0100

    Popravljeni manji problemi, Baza korisnika pretvorena u object

--- Software/app/src/main/java/com/example/receptomat/LoginActivity.kt
+++ Software/app/src/main/java/com/example/receptomat/LoginActivity.kt
@@ -7,22 +7,14 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.constraintlayout.widget.ConstraintLayout
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import database.BazaKorisnika
 
 class LoginActivity : AppCompatActivity() {
-
-    private val bazaKorisnika = BazaKorisnika()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_login)
 
-        val loginActivity = findViewById<ConstraintLayout>(R.id.loginActivity)
         val prijavaButton = findViewById<Button>(R.id.login_Button)
         val odustaniButton = findViewById<Button>(R.id.cancel_Button)
         val etUsername = findViewById<EditText>(R.id.usernameEditText)
@@ -33,7 +25,7 @@ class LoginActivity : AppCompatActivity() {
             val username = etUsername.text.toString()
             val password = etPassword.text.toString()
 
-            if (bazaKorisnika.provjeriKorisnika(username, password)) {
+            if (BazaKorisnika.provjeriKorisnika(username, password)) {
                 val intent = Intent(this, RecipesActivity::class.java)
                 startActivity(intent)
             } else {
@@ -44,19 +36,13 @@ class LoginActivity : AppCompatActivity() {
         }
 
         txtDontHaveAccount.setOnClickListener {
-            val intent = Intent(baseContext, RegisterActivity::class.java)
+            val intent = Intent(this, RegisterActivity::class.java)
             startActivity(intent)
         }
 
         odustaniButton.setOnClickListener {
-            val intent = Intent(baseContext, MainActivity::class.java)
+            val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
-        }
-
-        ViewCompat.setOnApplyWindowInsetsListener(loginActivity) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
         }
     }
 }
